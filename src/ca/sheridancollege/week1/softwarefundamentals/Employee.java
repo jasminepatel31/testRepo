eba90c896e7e929e99b3b1ce2792a0889d46832d
jasmine <jasmine@JP>
2019-05-13 14:24:39 -0400

    My second commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Employee.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Employee.java
@@ -1,4 +1,4 @@
-/*
+/* Line #1 added
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
